b4a285085640ef96772095ef7294402a67be46a4
Florence Rotich <kuravalley@gmail.com>
2023-02-21 16:58:02 +0300

    function that prints the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabets 'a - z'
+ */
+/*
+1. Declare a variable c of type char.
+2. Initialize c to the value of ‘a’.
+3. While c is less than or equal to ‘z’, do the following:
+	a. Print the value of c.
+	b. Increment c by 1.
+4. Print a new line.
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
