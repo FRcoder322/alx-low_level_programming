24de29812a6beb31d6eb618d01ee2ae7435f490f
Florence Rotich <kuravalley@gmail.com>
2023-02-21 17:44:33 +0300

    function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks whether alphabets character
+ * @c: int
+ * description - return 1 if upper or lower
+ * Return: 1 if alphabet 0 otherwise
+ */
+/*
+* If the character is between 97 and 122, it’s lowercase.
+* If the character is between 65 and 90, it’s uppercase.
+* If the character is neither of the above, it’s not a letter.
+*/
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
