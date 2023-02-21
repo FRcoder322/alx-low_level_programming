78f39585eec99d7b61229e8a9bddaff7b07dc1a6
Florence Rotich <kuravalley@gmail.com>
2023-02-21 12:21:41 +0300

    0-putchar.c exit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	char sh[8] = "_putchar";
