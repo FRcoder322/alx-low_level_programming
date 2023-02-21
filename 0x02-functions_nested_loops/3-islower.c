b861cb0d4ec40f22c97efd87b5732b120efa1a0d
Florence Rotich <kuravalley@gmail.com>
2023-02-21 21:26:51 +0300

    function that checks for lowercase character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,9 +9,9 @@
  */
 /*
  * If the character is between 97 and 122, then it is a lowercase letter.
- * If the character is not between 97 and 122, then it is not a lowercase letter.
+ * If the character is not between 97 and 122,it is not a lowercase letter.
 */
-   int _islower(int c)
+int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
