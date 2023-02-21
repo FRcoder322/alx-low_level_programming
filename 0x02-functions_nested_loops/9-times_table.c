85d2858b44a566971e72bc4e9e4cb957d9320b10
Florence Rotich <kuravalley@gmail.com>
2023-02-21 21:33:36 +0300

    unction that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,8 +8,8 @@
 * Create a nested for loop that will iterate through the columns of the table.
 * Multiply the row and column to get the product.
 * If the column is 0, print the product.
-* product is less than 10 and the column is not 0, print comma,two spaces,the product.
-* product is greater than or equal to 10, print comma,space, digit of the product.
+* product is less than 10 and the column is not.
+* product is greater than or equal to 10.
 * Print a new line after each row.
 */
 void times_table(void)
