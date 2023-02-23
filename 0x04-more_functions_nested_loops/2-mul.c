a77de297e6df20601f5acfb7510b6467f5f32d6d
Florence Rotich <kuravalley@gmail.com>
2023-02-23 23:13:19 +0300

    function that prints the numbers, from 0 to 9,

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *mul - function that multiplies two numbers
+ *@a: first number
+ *@b: second number
+ *Return: returns result
+ */
+
+/*
+1. It’s defining a function called mul that takes two integers as arguments and returns an integer.
+2. It’s returning the product of the two arguments.
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
