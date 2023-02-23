95e555f34c350bafc5a225821325fd28a317a9f3
Florence Rotich <kuravalley@gmail.com>
2023-02-23 23:46:16 +0300

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+/*
+* If the number is negative, print a minus sign and make the number positive.
+* Find the number of digits in the number.
+* Print the number.
+*/
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
