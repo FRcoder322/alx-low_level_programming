e5e1fe339de5b3354c95d0effeecc6156b9a0eb7
FRcoder322 <kuravalley@gmail.com>
2023-03-02 18:59:09 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,20 +1,32 @@
-char *_strcat(char *dest, char *src) {
-    // Find the end of the destination string
-    char *ptr = dest;
-    while (*ptr != '\0') {
-        ptr++;
-    }
-    
-    // Append the source string to the destination string
-    while (*src != '\0') {
-        *ptr = *src;
-        ptr++;
-        src++;
-    }
-    
-    // Add the terminating null byte
-    *ptr = '\0';
-    
-    // Return a pointer to the destination string
-    return dest;
-}
+#include "main.h"
+
+/**
+ * _strcat - concatenates the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will append
+ *@src: string that will be concatenated upon
+ *
+ * Return: returns pointer to @dest
+ */
+
+/*
+*First, we’re declaring a function called _strcat that takes two arguments: dest and src.
+*Next, we’re declaring two variables: index and dest_len.
+*Then, we’re using a while loop to iterate through the dest string and find its length.
+*Next, we’re using a for loop to iterate through the src string and append each character to the end of the dest string.
+*Finally, we’re returning the dest string.
+*/
+/*
+Time Complexity: O(n)
+*/
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}                                                                                     
