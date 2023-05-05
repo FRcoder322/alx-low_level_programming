1e40127899288c679d47769d5bb5e5a98fbf6527
Florence Rotich <kuravalley@gmail.com>
2023-05-05 19:44:59 +0300

    print binary

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,17 +10,17 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int k, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+int k, count = 0;
+unsigned long int current;
+unsigned long int exclusive = n ^ m;
 
-	for (k= 63; k >= 0; k--)
-	{
-		current = exclusive >> k;
-		if (current & 1)
-			count++;
+for (k= 63; k >= 0; k--)
+{
+current = exclusive >> k;
+if (current & 1)
+count++;
 
-	}
+}
 
-	return (count);
+return (count);
 }
