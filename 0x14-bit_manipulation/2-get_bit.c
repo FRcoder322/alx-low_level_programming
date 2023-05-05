03fd6afb78eafc32891360fe91fbabd04656106f
Florence Rotich <kuravalley@gmail.com>
2023-05-05 19:28:07 +0300

    get bits

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
-	/**
-	*get_bit  is used to get the value of a bit at the given index
-	*@n is the  the number in the index
-	*@index is the index starting from 0 of the bit required
-	*
-	*Return returns  the converted value
-	*/
+/**
+ * get_bit - returns the value of a bit at an index in a decimal number
+ * @n: number to search
+ * @index: index of the bit
+ *
+ * Return: value of the bit
+ */
 
- int get_bit(unsigned long int n, unsigned int index)
+
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 50)
+	int bit_val;
+
+	if (index > 63)
 		return (-1);
 
-	return ((n >> index) & 1);
+	bit_val = (n >> index) & 1;
+
+	return (bit_val);
 }
