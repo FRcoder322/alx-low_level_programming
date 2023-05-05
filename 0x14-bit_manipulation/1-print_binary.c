65527f5717cc0ca52963250a0564114ba1e453b7
Florence Rotich <kuravalley@gmail.com>
2023-05-05 20:15:18 +0300

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -15,12 +15,12 @@ void print_binary(unsigned long int n)
 
 		if (current & 1)
 		{
-_putchar('1');
+    putchar('1');
 		count++;
 		}
 		else if (count)
-			_putchar('0');
+			putchar('0');
 	}
 	if (!count)
-	_putchar('0');
+	putchar('0');
 }
